2f116dd8aaed688eefc0ebeb346691aa2c5b80a3
Ethan Welsh <ewelsh@google.com>
2022-12-04 12:04:31 -0800

    Solve day 3

--- src/Days/Day03.hs
+++ src/Days/Day03.hs
@@ -10,6 +10,7 @@ import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
+import Control.Applicative ((<|>))
 
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
@@ -21,10 +22,13 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = (many1 (notChar '\n')) `sepBy` endOfLine
+inputParser = (many1 (char '0' <|> char '1')) `sepBy` endOfLine
 
 ------------ TYPES ------------
-type Input = [String]
+type Bit = Char
+type Byte = String
+
+type Input = [Byte]
 
 type OutputA = Int
 
@@ -32,29 +36,70 @@ type OutputB = Int
 
 ------------ PART A ------------
 
-splitInHalf :: String -> (String, String)
-splitInHalf s = splitAt ((length s) `div`  2) s
+byteToInt :: Byte -> Int
+byteToInt bits = let
+    powersOfTwo = [2^x | x <- [0..]]
+    zipped = zip (reverse bits) powersOfTwo :: [(Bit, Int)]
+    onlyOn = filter ((=='1') . fst) zipped  :: [(Bit, Int)]
+    totals = map snd onlyOn                 :: [Int]
+    in sum totals
 
-inCommon :: (String, String) -> String
-inCommon (a, b) = Set.toList $ Set.intersection (Set.fromList a) (Set.fromList b)
+keepWhereBitInPosEquals :: [Byte] -> Int -> Bit -> [Byte]
+keepWhereBitInPosEquals bs pos target = let
+    getBit b = b !! pos
+    bitMatches b = (getBit b) == target
+    in filter bitMatches bs
 
-score :: Char -> Int
-score c = let charToScore = Map.fromList $ zip (['a'..'z']++['A'..'Z']) [1..52] 
-          in charToScore Map.! c
+getCounts :: [Byte] -> Int -> (Int, Int)
+getCounts bs pos = let
+    zeroCount = length $ keepWhereBitInPosEquals bs pos '0'
+    oneCount = length $ keepWhereBitInPosEquals bs pos '1'
+    in (zeroCount, oneCount)
 
-scoreLine :: String -> Int
-scoreLine l = sum $ map score $ inCommon $ splitInHalf l
+pickBigger :: ([Bit], [Bit]) -> Bit
+pickBigger (zeros, ones) = if (length zeros) > (length ones) then '0' else '1'
+
+pickSmaller :: ([Bit], [Bit]) -> Bit
+pickSmaller (zeros, ones) = if (length zeros) < (length ones) then '0' else '1'
+
+pickGamma :: [Byte] -> Byte
+pickGamma bs = let
+    transposed = transpose bs                                                  :: [Byte]
+    partitioned = map (partition (=='0')) transposed                           :: [([Bit], [Bit])]
+    in map pickBigger partitioned
+
+pickEpsilon :: [Byte] -> Byte
+pickEpsilon bs = let
+    transposed = transpose bs                                                  :: [Byte]
+    partitioned = map (partition (=='0')) transposed                           :: [([Bit], [Bit])]
+    in map pickSmaller partitioned
 
 partA :: Input -> OutputA
-partA input = sum $ map scoreLine input
+partA bs = byteToInt (pickGamma bs) * byteToInt (pickEpsilon bs)
 
 ------------ PART B ------------
-groupsOfThree :: [String] -> [(String, String, String)]
-groupsOfThree [] = []
-groupsOfThree (a:b:c:xs) = (a, b, c) : groupsOfThree xs
 
-inCommonThree :: (String, String, String) -> Char
-inCommonThree (a, b, c) = head $ inCommon (inCommon (a, b), c)
+pickOxygen :: [Byte] -> Byte
+pickOxygen bs = pickOxygenHelp bs 0
+
+pickOxygenHelp :: [Byte] -> Int -> Byte
+pickOxygenHelp [b] _ = b
+pickOxygenHelp bs pos = let
+    counts = getCounts bs pos
+    target = if snd counts >= fst counts then '1' else '0'
+    filtered = keepWhereBitInPosEquals bs pos target
+    in pickOxygenHelp filtered (pos+1)
+
+pickCarbon :: [Byte] -> Byte
+pickCarbon bs = pickCarbonHelp bs 0
+
+pickCarbonHelp :: [Byte] -> Int -> Byte
+pickCarbonHelp [b] _ = b
+pickCarbonHelp bs pos = let
+    counts = getCounts bs pos
+    target = if fst counts <= snd counts then '0' else '1'
+    filtered = keepWhereBitInPosEquals bs pos target
+    in pickCarbonHelp filtered (pos+1)
 
 partB :: Input -> OutputB
-partB input = sum $ map (score . inCommonThree) (groupsOfThree input)
+partB bs = byteToInt (pickCarbon bs) * byteToInt (pickOxygen bs)
